b15c4ad0fae419b973f8ac33b591cb4c2ac857da
reem gamal <reem_gamal2008@yahoo.com>
2017-01-30 01:06:55 +0200

    make the lastwordlength function

---
+++ assignments/Assignment 8/strings.js
@@ -0,0 +1,15 @@
+exports.lengthOfLastWord = function(str)
+{   
+    var arr=[];
+    arr = str.split(' ');
+    arr = arr.map(String);
+    var i = arr.length;
+    if(str.length === 0)
+    {return 0;}
+    else { 
+        while(isNaN(arr[i-1])==false) 
+        {  i=i-1 ;
+        }
+        return arr[i-1].length;
+    }
+}
