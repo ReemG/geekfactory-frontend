51f49b2e3fdc12aee39af7fdb5f42a017d2ba227
reem gamal <reem_gamal2008@yahoo.com>
2017-02-12 00:53:20 +0200

    Correct remove function on task manager and some other mistakes

--- assignments/assignment11/task-manager.js
+++ assignments/assignment11/task-manager.js
@@ -1,60 +1,63 @@
-var taskManager = {
-    create: function () {
-        array = [];
-        manager = {
-            create: function (category, title, priority, estimate) {
-                this.category = category;
-                this.title = title;
-                this.priority = priority;
-                this.estimate = estimate;
-                this.spent = 0;
-                this.remaining = 1.5;
-                array.push(this);
-                return this;
-            },
-
-            get: function (value) {
-                return array[value];
-            },
-
-            getAll: function (active) {
-                if (active === true) {
-                    for (var i = 0; i < array.length; i++) {
-                        if (array[i].estimate === 0)
-                        { array.splice(i, 1); }
-                    }
-                }
-                return array;
-            },
-
-            find: function (matcher) {
-                var arr = [];
-                for (var i = 0; i < array.length; i++) {
-                    var t = true;
-                    var c = true;
-                    var lowertitle = array[i].title.toLowerCase();
-                    var lowercategory = array[i].category.toLowerCase();
-                    for (var j = 0; j < matcher.length; j++) {
-                        if (matcher.charAt(j) !== lowercategory.charAt(j))
-                        { c = false; }
-                        if (matcher.charAt(j) !== lowertitle.charAt(j))
-                        { t = false; }
-                    }
-                    if (t === true || c === true)
-                    { arr.push(array[i]); }
-                }
-                return arr;
-            },
-
-            remove : function(index){
-                return array.splice(index,1);
+var Task = require('./task')
+var createTaskManager = function () {
+    tasks = [];
+    create = function (category, title, priority, estimate) {
+        var task = new Task(category, title, priority, estimate);
+        tasks.push(task);
+        return task;
+    }
+    get = function (value) {
+        return tasks[value];
+    }
+    getAll = function (active) {
+        if (active === true) {
+            for (var i = 0; i < tasks.length; i++) {
+                if (tasks[i].estimate === 0)
+                { tasks.splice(i, 1); }
             }
-        };
-
-        return manager;
+        }
+        return tasks;
+    }
+    find = function (matcher) {
+        var arr = [];
+        for (var i = 0; i < tasks.length; i++) {
+            var t = true;
+            var c = true;
+            var lowertitle = tasks[i].title.toLowerCase();
+            var lowercategory = tasks[i].category.toLowerCase();
+            for (var j = 0; j < matcher.length; j++) {
+                if (matcher.charAt(j) !== lowercategory.charAt(j))
+                { c = false; }
+                if (matcher.charAt(j) !== lowertitle.charAt(j))
+                { t = false; }
+            }
+            if (t === true || c === true)
+            { arr.push(tasks[i]); }
+        }
+        return arr;
     }
 
-};
+    remove = function (index) {
+        if (typeof index !== 'number') {
+            for (var i = 0; i < tasks.length; i++) {
+                if (tasks[i] === index) {
+                    index = i;
+                    break;
+                }
+            }
+        }
 
+        if (index >= 0 && index < tasks.length) {
+            tasks.splice(index, 1);
+        }
+    }
 
-module.exports = taskManager;
+    return {
+        create: create,
+        get: get,
+        getAll: getAll,
+        find: find,
+        remove: remove
+    };
+}
+module.exports.create = createTaskManager;
